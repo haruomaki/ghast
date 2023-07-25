0f0f1a89a900bb052202741f93d79b350278dddf
Haruo <haruo-mtok@outlook.com>
2023-07-25 22:15:34 +0900

    AA/AB

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,103 @@
-use std::str::Chars;
-
 #[derive(Debug)]
 enum Expression {
     Terminal(char),
+    NonTerminal(String),
     Sequence(Box<Expression>, Box<Expression>),
+    OrderedChoice(Box<Expression>, Box<Expression>),
 }
 
-struct Parser<'a> {
-    str_iter: Chars<'a>,
-}
-
-impl<'a> Parser<'a> {
-    fn new(input: &'a str) -> Parser<'a> {
-        Parser {
-            str_iter: input.chars(),
-        }
-    }
-
-    fn parse_expression(&mut self, expression: &Expression) -> bool {
-        match expression {
+impl Expression {
+    fn parse(&self, grammar: &Grammar, p: &mut Parser) -> bool {
+        println!("parse: {:?}, i = {}", self, p.i);
+        let i = p.i;
+        let result = match self {
             Expression::Terminal(c) => {
-                if let Some(next_char) = self.str_iter.next() {
-                    return next_char == *c;
+                if p.str.chars().nth(p.i) == Some(*c) {
+                    p.i += 1;
+                    true
+                } else {
+                    false
                 }
-                false
             }
-            Expression::Sequence(e1, e2) => {
-                self.parse_expression(&e1) && self.parse_expression(&e2)
-            }
+            Expression::NonTerminal(sym) => grammar.parse_symbol(sym, p),
+            Expression::Sequence(e1, e2) => e1.parse(grammar, p) && e2.parse(grammar, p),
+            Expression::OrderedChoice(e1, e2) => e1.parse(grammar, p) || e2.parse(grammar, p),
+        };
+
+        if !result {
+            p.i = i
+        };
+
+        println!("end: {:?}, i = {}, result = {}", self, p.i, result);
+        result
+    }
+}
+
+#[derive(Debug)]
+struct Grammar {
+    rules: Vec<(String, Expression)>,
+    start_symbol: String,
+}
+
+impl Grammar {
+    fn new(start_symbol: &str) -> Self {
+        Grammar {
+            rules: Vec::new(),
+            start_symbol: start_symbol.to_string(),
         }
     }
+
+    fn add_rule(&mut self, sym: &str, expr: Expression) {
+        self.rules.push((sym.to_string(), expr));
+    }
+
+    fn parse(&self, p: &mut Parser) -> bool {
+        self.parse_symbol(&self.start_symbol, p) && p.i == p.str.len()
+    }
+
+    fn parse_symbol(&self, symbol: &str, p: &mut Parser) -> bool {
+        if let Some(expr) = self
+            .rules
+            .iter()
+            .find(|(sym, _)| *sym == symbol)
+            .map(|(_, e)| e)
+        {
+            expr.parse(self, p)
+        } else {
+            false
+        }
+    }
+}
+
+struct Parser {
+    str: String,
+    i: usize,
 }
 
 fn main() {
-    let input = "ab";
-    let mut parser = Parser::new(input);
-    let expression = Expression::Sequence(
-        Box::new(Expression::Terminal('a')),
-        Box::new(Expression::Terminal('b')),
-    );
+    let input = String::from("ab");
+    let mut parser = Parser { str: input, i: 0 };
 
-    if parser.parse_expression(&expression) {
-        println!("成功");
+    let mut grammar = Grammar::new("S");
+    grammar.add_rule(
+        "S",
+        Expression::OrderedChoice(
+            Box::new(Expression::Sequence(
+                Box::new(Expression::NonTerminal("A".to_string())),
+                Box::new(Expression::NonTerminal("A".to_string())),
+            )),
+            Box::new(Expression::Sequence(
+                Box::new(Expression::NonTerminal("A".to_string())),
+                Box::new(Expression::NonTerminal("B".to_string())),
+            )),
+        ),
+    );
+    grammar.add_rule("A", Expression::Terminal('a'));
+    grammar.add_rule("B", Expression::Terminal('b'));
+
+    if grammar.parse(&mut parser) {
+        println!("Matched!");
     } else {
-        println!("失敗");
+        println!("Not matched");
     }
 }
