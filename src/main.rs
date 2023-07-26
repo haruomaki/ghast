f551d97703053c7e7c27a3291117d9f083f984b6
Haruo <haruo-mtok@outlook.com>
2023-07-26 19:39:39 +0900

    clean

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-// #[derive(Debug)]
-// enum ParseStatus {
-//     Continue,
-//     Accept,
-//     Reject,
-// }
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Expression {
@@ -14,80 +9,66 @@ enum Expression {
 }
 
 impl Expression {
-    fn parse(&self, grammar: &Grammar, str: &String, i: usize) -> Option<usize> {
-        println!("parse: {:?}, i = {}", self, i);
+    fn parse(&self, grammar: &Grammar, input: &str, i: usize) -> Option<usize> {
         let result = match self {
             Expression::Terminal(c) => {
-                if str.chars().nth(i) == Some(*c) {
+                if input.chars().nth(i) == Some(*c) {
                     Some(i + 1)
                 } else {
                     None
                 }
             }
-            Expression::NonTerminal(sym) => grammar.parse_symbol(sym, str, i),
+            Expression::NonTerminal(symbol) => {
+                let expr = grammar.rules.get(symbol)?;
+                expr.parse(grammar, input, i)
+            }
             Expression::Sequence(e1, e2) => {
-                if let Some(j) = e1.parse(grammar, str, i) {
-                    e2.parse(grammar, str, j)
-                } else {
-                    None
-                }
+                let j = e1.parse(grammar, input, i)?;
+                e2.parse(grammar, input, j)
             }
             Expression::OrderedChoice(e1, e2) => {
-                if let Some(j) = e1.parse(grammar, str, i) {
+                if let Some(j) = e1.parse(grammar, input, i) {
                     Some(j)
                 } else {
-                    e2.parse(grammar, str, i)
+                    e2.parse(grammar, input, i)
                 }
             }
         };
 
-        println!("end: {:?}, i = {}, result = {:?}", self, i, result);
         result
     }
 }
 
 #[derive(Debug)]
 struct Grammar {
-    rules: Vec<(String, Expression)>,
+    rules: HashMap<String, Expression>,
     start_symbol: String,
 }
 
 impl Grammar {
     fn new(start_symbol: &str) -> Self {
         Grammar {
-            rules: Vec::new(),
+            rules: HashMap::new(),
             start_symbol: start_symbol.to_string(),
         }
     }
 
     fn add_rule(&mut self, sym: &str, expr: Expression) {
-        self.rules.push((sym.to_string(), expr));
+        self.rules.insert(sym.to_string(), expr);
     }
 
-    fn parse(&self, str: &String) -> bool {
-        if let Some(t) = self.parse_symbol(&self.start_symbol, str, 0) {
-            t == str.len()
+    fn parse(&self, input: &str) -> bool {
+        let master_expr = Expression::NonTerminal(self.start_symbol.clone());
+        if let Some(t) = master_expr.parse(self, input, 0) {
+            t == input.len()
         } else {
             false
-        }
-    }
-
-    fn parse_symbol(&self, symbol: &str, str: &String, i: usize) -> Option<usize> {
-        if let Some(expr) = self
-            .rules
-            .iter()
-            .find(|(sym, _)| *sym == symbol)
-            .map(|(_, e)| e)
-        {
-            expr.parse(self, str, i)
-        } else {
-            None
         }
     }
 }
 
 fn main() {
-    let str = String::from("aab");
+    let input = String::from("aab");
 
     let mut grammar = Grammar::new("S");
     grammar.add_rule(
@@ -95,11 +76,11 @@ fn main() {
         Expression::OrderedChoice(
             Box::new(Expression::Sequence(
                 Box::new(Expression::NonTerminal("A".to_string())),
-                Box::new(Expression::NonTerminal("A".to_string())),
+                Box::new(Expression::NonTerminal("B".to_string())),
             )),
             Box::new(Expression::Sequence(
                 Box::new(Expression::NonTerminal("A".to_string())),
-                Box::new(Expression::NonTerminal("B".to_string())),
+                Box::new(Expression::NonTerminal("A".to_string())),
             )),
         ),
     );
@@ -112,9 +93,9 @@ fn main() {
         ),
     );
 
-    if grammar.parse(&str) {
-        println!("Matched!");
+    if grammar.parse(&input) {
+        println!("受理！");
     } else {
-        println!("Not matched");
+        println!("拒否…");
     }
 }
