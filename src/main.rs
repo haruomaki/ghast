b52b632a8388af62caff3b3a4e2798aaca3b09f2
Haruo <haruo-mtok@outlook.com>
2023-09-06 23:21:34 +0900

    bind

--- src/main.rs
+++ src/main.rs
@@ -10,41 +10,43 @@ enum ParseError {
     WrongTerminal,
     MissingNonTerminal,
     IncompleteParse,
+    IterationError,
 }
 
 type ParseResult<T> = Result<T, ParseError>;
 
 struct Parser<T> {
-    _parse: Box<dyn Fn(std::str::Chars) -> ParseResult<T>>,
+    _parse: Box<dyn Fn(&mut std::str::Chars) -> ParseResult<T>>,
+}
+
+impl<T> Parser<T> {
+    fn parse(&self, input: impl AsRef<str>) -> ParseResult<T> {
+        let mut iter = input.as_ref().chars();
+        (self._parse)(&mut iter)
+    }
 }
 
 impl<T: 'static> Parser<T> {
-    fn parse(&self, input: impl AsRef<str>) -> ParseResult<T> {
-        let iter = input.as_ref().chars();
-        (self._parse)(iter)
+    fn bind<S: 'static>(self, f: fn(T) -> Parser<S>) -> Parser<S> {
+        Parser {
+            _parse: Box::new(move |mut iter| {
+                let res = (self._parse)(&mut iter)?;
+                let par = f(res);
+                (par._parse)(&mut iter)
+            }),
+        }
     }
-
-    // fn bind<S: 'static>(mut self, f: fn(T) -> Parser<S>) -> Parser<S> {
-    //     Parser {
-    //         _parse: Box::new(move |mut iter| {
-    //             let res = (self._parse)(iter.clone())?;
-    //             let mut par = f(res);
-    //             (par._parse)(iter)
-    //         }),
-    //     }
-    // }
 }
 
 impl Parser<()> {
     fn terminal(test: char) -> Self {
         Parser {
-            _parse: Box::new(move |mut iter| {
-                if let Some(c) = iter.next() {
-                    if c == test {
-                        return Ok(());
-                    }
-                }
-                Err(ParseError::WrongTerminal)
+            _parse: Box::new(move |iter| match iter.next() {
+                Some(c) => match c == test {
+                    true => return Ok(()),
+                    false => Err(ParseError::WrongTerminal),
+                },
+                None => Err(ParseError::IterationError),
             }),
         }
     }
@@ -54,6 +56,10 @@ fn main() {
     let input = "Hello, World!";
 
     let parser = Parser::terminal('H');
+    let parser = parser.bind(|()| Parser::terminal('e'));
+    let parser = parser.bind(|()| Parser::terminal('l'));
+    let parser = parser.bind(|()| Parser::terminal('l'));
+    let parser = parser.bind(|()| Parser::terminal('o'));
     let result = parser.parse(input);
     println!("{:?}", result);
 }
