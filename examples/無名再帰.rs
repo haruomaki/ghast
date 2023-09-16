8206544d8286258562c672798f06badcc52d854c
Haruo <haruo-mtok@outlook.com>
2023-09-16 16:25:34 +0900

    closure in fix

--- examples/無名再帰.rs
+++ examples/無名再帰.rs
@@ -9,24 +9,16 @@ fn fix(source: impl Fn(Rc<dyn Fn(u32) -> u32>, u32) -> u32 + 'static) -> Rc<dyn 
     let weak_holder: Rc<RefCell<Weak<dyn Fn(u32) -> u32>>> =
         Rc::new(RefCell::new(Weak::<fn(u32) -> u32>::new()));
     let weak_holder2 = weak_holder.clone();
-    let fact: Rc<dyn Fn(u32) -> u32> = Rc::new(move |x| {
-        let fact = weak_holder2.borrow().upgrade().unwrap();
-        source(fact, x)
+    let f: Rc<dyn Fn(u32) -> u32> = Rc::new(move |x| {
+        let f = weak_holder2.borrow().upgrade().unwrap();
+        source(f, x)
     });
-    weak_holder.replace(Rc::downgrade(&fact));
-    fact
+    weak_holder.replace(Rc::downgrade(&f));
+    f
 }
 
 fn main() {
-    let source = |fact: Rc<dyn Fn(u32) -> u32>, x| {
-        if x == 0 {
-            1
-        } else {
-            x * fact(x - 1)
-        }
-    };
-
-    let fact = fix(source);
+    let fact = fix(|fact, x| if x == 0 { 1 } else { x * fact(x - 1) });
 
     println!("{}", fact(5)); // prints "120"
     println!("{}", fact(6)); // prints "720"
