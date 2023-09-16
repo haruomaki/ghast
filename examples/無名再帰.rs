b0d8aa1d4a9c72bf4063560db5f5132db63dd3c8
Haruo <haruo-mtok@outlook.com>
2023-09-16 15:47:20 +0900

    fix関数

--- examples/無名再帰.rs
+++ examples/無名再帰.rs
@@ -5,6 +5,18 @@ use std::{
     rc::{Rc, Weak},
 };
 
+fn fix(source: impl Fn(Rc<dyn Fn(u32) -> u32>, u32) -> u32 + 'static) -> Rc<dyn Fn(u32) -> u32> {
+    let weak_holder: Rc<RefCell<Weak<dyn Fn(u32) -> u32>>> =
+        Rc::new(RefCell::new(Weak::<fn(u32) -> u32>::new()));
+    let weak_holder2 = weak_holder.clone();
+    let fact: Rc<dyn Fn(u32) -> u32> = Rc::new(move |x| {
+        let fact = weak_holder2.borrow().upgrade().unwrap();
+        source(fact, x)
+    });
+    weak_holder.replace(Rc::downgrade(&fact));
+    fact
+}
+
 fn main() {
     let source = |fact: Rc<dyn Fn(u32) -> u32>, x| {
         if x == 0 {
@@ -14,14 +26,7 @@ fn main() {
         }
     };
 
-    let weak_holder: Rc<RefCell<Weak<dyn Fn(u32) -> u32>>> =
-        Rc::new(RefCell::new(Weak::<fn(u32) -> u32>::new()));
-    let weak_holder2 = weak_holder.clone();
-    let fact: Rc<dyn Fn(u32) -> u32> = Rc::new(move |x| {
-        let fact = weak_holder2.borrow().upgrade().unwrap();
-        source(fact, x)
-    });
-    weak_holder.replace(Rc::downgrade(&fact));
+    let fact = fix(source);
 
     println!("{}", fact(5)); // prints "120"
     println!("{}", fact(6)); // prints "720"
