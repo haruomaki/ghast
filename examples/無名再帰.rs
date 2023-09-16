6a3ee4846902a3f9808703c1c1f4a96002a767e7
Haruo <haruo-mtok@outlook.com>
2023-09-16 15:43:48 +0900

    factを受け取るクロージャ

--- examples/無名再帰.rs
+++ examples/無名再帰.rs
@@ -6,16 +6,20 @@ use std::{
 };
 
 fn main() {
-    let weak_holder: Rc<RefCell<Weak<dyn Fn(u32) -> u32>>> =
-        Rc::new(RefCell::new(Weak::<fn(u32) -> u32>::new()));
-    let weak_holder2 = weak_holder.clone();
-    let fact: Rc<dyn Fn(u32) -> u32> = Rc::new(move |x| {
-        let fact = weak_holder2.borrow().upgrade().unwrap();
+    let source = |fact: Rc<dyn Fn(u32) -> u32>, x| {
         if x == 0 {
             1
         } else {
             x * fact(x - 1)
         }
+    };
+
+    let weak_holder: Rc<RefCell<Weak<dyn Fn(u32) -> u32>>> =
+        Rc::new(RefCell::new(Weak::<fn(u32) -> u32>::new()));
+    let weak_holder2 = weak_holder.clone();
+    let fact: Rc<dyn Fn(u32) -> u32> = Rc::new(move |x| {
+        let fact = weak_holder2.borrow().upgrade().unwrap();
+        source(fact, x)
     });
     weak_holder.replace(Rc::downgrade(&fact));
 
