b1def55bc5e636ae0741fde10bc84b4cf216f36d
Haruo <haruo-mtok@outlook.com>
2023-10-26 13:55:50 +0900

    はじめてのinkwell

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use inkwell::context::Context;
 use monapa::*;
+use std::error::Error;
 use std::io::{self, Write};
 
 #[allow(dead_code)]
@@ -44,6 +46,8 @@ fn ghast_master() -> Parser<Ghast> {
 }
 
 fn main() {
+    build_hi().unwrap();
+
     print!("入力: ");
     io::stdout().flush().unwrap();
     let input = {
@@ -67,4 +71,42 @@ fn main() {
             }
         }
     }
+}
+
+// https://yhara.jp/2019/06/09/inkwell-hi
+fn build_hi() -> Result<(), Box<dyn Error>> {
+    let context = Context::create();
+    let module = context.create_module("main");
+    let builder = context.create_builder();
+    let i32_type = context.i32_type();
+
+    // declare i32 @putchar(i32)
+    let putchar_type = i32_type.fn_type(&[i32_type.into()], false);
+    module.add_function("putchar", putchar_type, None);
+
+    // define i32 @main() {
+    let main_type = i32_type.fn_type(&[], false);
+    let function = module.add_function("main", main_type, None);
+    let basic_block = context.append_basic_block(function, "entry");
+    builder.position_at_end(basic_block);
+
+    // call i32 @putchar(i32 72)
+    let fun = module.get_function("putchar");
+    builder.build_call(
+        fun.unwrap(),
+        &[i32_type.const_int(72, false).into()],
+        "putchar",
+    )?;
+    builder.build_call(
+        fun.unwrap(),
+        &[i32_type.const_int(105, false).into()],
+        "putchar",
+    )?;
+
+    // ret i32 0
+    builder.build_return(Some(&i32_type.const_int(0, false)))?;
+
+    module.print_to_stderr();
+
+    Ok(())
 }
