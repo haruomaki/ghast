0e3ab8aaa690c7cbc8a8d290520ec59f5749300d
Haruo <haruo-mtok@outlook.com>
2024-06-07 01:13:07 +0900

    [new] parser.rsにファイル分割

--- src/main.rs
+++ src/main.rs
@@ -3,88 +3,11 @@ use inkwell::context::Context;
 use inkwell::module::Module;
 use inkwell::AddressSpace;
 
-use monapa::*;
 use std::error::Error;
 use std::io::{self, Write};
 
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-enum Ghast {
-    Symbol(String),
-    Fn(String, Box<Ghast>),
-    Apply(Box<Ghast>, Box<Ghast>),
-    I32(i32),
-}
-
-fn id_start() -> Parser<char> {
-    Parser::satisfy(|c| c == '_' || c.is_alphabetic())
-}
-
-fn id_continue() -> Parser<char> {
-    Parser::satisfy(|c| c == '_' || c.is_alphanumeric())
-}
-
-fn id() -> Parser<String> {
-    pdo! {
-        start <- id_start();
-        conti <- id_continue() * ..;
-        let idvec = vec![vec![start], conti].concat();
-        return idvec.iter().collect()
-    }
-}
-
-fn literal_digit() -> Parser<char> {
-    Parser::satisfy(|c| c.is_ascii_digit())
-}
-
-fn ghast_symbol() -> Parser<Ghast> {
-    id().bind(|id| Parser::ret(Ghast::Symbol(id)))
-}
-
-fn ghast_fn() -> Parser<Ghast> {
-    pdo! {
-        single('\\');
-        arg <- id();
-        whitespace() * (..);
-        chunk("->");
-        whitespace() * (..);
-        cont <- ghast_master();
-        return Ghast::Fn(arg, Box::new(cont))
-    }
-}
-
-fn ghast_i32() -> Parser<Ghast> {
-    pdo! {
-        num <- literal_digit() * (1..);
-        let num_str = num.iter().collect::<String>();
-        return Ghast::I32(num_str.parse().unwrap())
-    }
-}
-
-fn ghast_apply_left() -> Parser<Ghast> {
-    ghast_fn() | ghast_symbol() | ghast_i32()
-}
-
-fn ghast_apply_right() -> Parser<Option<Ghast>> {
-    // FIXME: 余計なカッコを明示しないといけないバグを修正
-    (pdo! {
-        whitespace() * (1..);
-        left <- ghast_apply_left();
-        return Some(left)
-    }) | Parser::ret(None)
-}
-
-fn ghast_master() -> Parser<Ghast> {
-    pdo! {
-        // Applyの左再帰を除去した
-        left <- ghast_apply_left();
-        right <- ghast_apply_right();
-        return match right {
-            Some(right) => Ghast::Apply(Box::new(left), Box::new(right)),
-            None => left,
-        }
-    }
-}
+mod parser;
+use parser::{Ghast, ParseError};
 
 fn main() -> Result<(), ParseError> {
     eprint!("入力: ");
@@ -97,7 +20,7 @@ fn main() -> Result<(), ParseError> {
         buf.trim().to_string()
     };
 
-    let parser_master = ghast_master();
+    let parser_master = parser::ghast_master();
 
     match parser_master.parse(&input) {
         Ok(ast) => {
