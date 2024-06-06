0438bd2c47b3847830273c32d6866b481e771f86
Haruo <haruo-mtok@outlook.com>
2024-06-06 15:44:50 +0900

    グローバル変数宣言と関数宣言の方法を洗練

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use inkwell::builder::Builder;
 use inkwell::context::Context;
-use inkwell::module::{Linkage, Module};
-use inkwell::values::PointerValue;
+use inkwell::module::Module;
 use inkwell::AddressSpace;
 
 use monapa::*;
@@ -97,24 +96,6 @@ fn build_main() -> Result<String, Box<dyn Error>> {
     let module = context.create_module("main");
     let builder = context.create_builder();
     let i32_type = context.i32_type();
-    let i8_type = context.i8_type();
-    let i8ptr_type = i8_type.ptr_type(AddressSpace::default());
-
-    // Define the string constant
-    let str_constant = context.const_string(b"%d\n", true);
-    let global_str = module.add_global(
-        str_constant.get_type(),
-        Some(AddressSpace::default()),
-        "num_format",
-    );
-    global_str.set_initializer(&str_constant);
-    global_str.set_unnamed_addr(true);
-    global_str.set_constant(true);
-    global_str.set_linkage(Linkage::Private);
-
-    // declare i32 @printf(ptr, ...)
-    let printf_type = i32_type.fn_type(&[i8ptr_type.into()], true);
-    module.add_function("printf", printf_type, None);
 
     // define i32 @main() {
     let main_type = i32_type.fn_type(&[], false);
@@ -122,13 +103,7 @@ fn build_main() -> Result<String, Box<dyn Error>> {
     let basic_block = context.append_basic_block(function, "entry");
     builder.position_at_end(basic_block);
 
-    print_num(
-        &context,
-        &module,
-        &builder,
-        global_str.as_pointer_value(),
-        334,
-    );
+    print_num(&context, &module, &builder, 334);
 
     // ret i32 0
     builder.build_return(Some(&i32_type.const_int(0, false)))?;
@@ -136,21 +111,30 @@ fn build_main() -> Result<String, Box<dyn Error>> {
     Ok(module.print_to_string().to_string())
 }
 
-fn print_num(
-    context: &Context,
-    module: &Module,
-    builder: &Builder,
-    format_str: PointerValue,
-    value: i32,
-) {
+fn print_num<'ctx>(context: &'ctx Context, module: &Module<'ctx>, builder: &Builder, value: i32) {
     let i32_type = context.i32_type();
+    let i8_type = context.i8_type();
+    let i8ptr_type = i8_type.ptr_type(AddressSpace::default());
 
-    let fun = module.get_function("printf");
+    let format_str = module.get_global("num_format").unwrap_or_else(|| {
+        // 初回時に文字列リテラルをグローバル変数として追加
+        builder
+            .build_global_string_ptr("数値は%dです\n", "num_format")
+            .unwrap()
+    });
+
+    let fun = module.get_function("printf").unwrap_or_else(|| {
+        // declare i32 @printf(ptr, ...)
+        let printf_type = i32_type.fn_type(&[i8ptr_type.into()], true);
+        module.add_function("printf", printf_type, None)
+    });
+
+    // printfの呼び出し
     builder
         .build_call(
-            fun.unwrap(),
+            fun,
             &[
-                format_str.into(),
+                format_str.as_pointer_value().into(),
                 i32_type.const_int(value as u64, false).into(),
             ],
             "printf_result",
