ea9ca761a2995c22f0e0f7671f1ef4879f71e38d
Haruo <haruo-mtok@outlook.com>
2024-06-10 02:26:00 +0900

    Context, Module, Builderをひとまとめに

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use corelang::CoreLang;
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::module::Module;
-use inkwell::values::FunctionValue;
+use inkwell::values::{CallSiteValue, FunctionValue};
 use inkwell::AddressSpace;
 
 use std::error::Error;
@@ -12,6 +12,25 @@ mod ghast;
 mod operator;
 
 use ghast::{Ghast, Literal, ParseError};
+
+/// Context, Module, Builderをひとまとめにするための構造体
+#[derive(Debug)]
+pub struct CompileController<'ctx> {
+    pub context: &'ctx Context,
+    pub module: Module<'ctx>,
+    pub builder: Builder<'ctx>,
+}
+
+impl<'ctx> CompileController<'ctx> {
+    // コンストラクタ関数
+    pub fn new(context: &'ctx Context, module_name: impl AsRef<str>) -> Self {
+        CompileController {
+            context,
+            module: context.create_module(module_name.as_ref()),
+            builder: context.create_builder(),
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // 入力受け付け
@@ -45,8 +64,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 // https://yhara.jp/2019/06/09/inkwell-hi
 fn build_main(ast: CoreLang) -> Result<String, Box<dyn Error>> {
     let context = Context::create();
-    let module = context.create_module("main");
-    let builder = context.create_builder();
+    let ctr = CompileController::new(&context, "main");
+    let module = &ctr.module;
+    let builder = &ctr.builder;
     let i32_type = context.i32_type();
 
     // define i32 @main() {
@@ -55,7 +75,7 @@ fn build_main(ast: CoreLang) -> Result<String, Box<dyn Error>> {
     let basic_block = context.append_basic_block(function, "entry");
     builder.position_at_end(basic_block);
 
-    translate(&context, &module, &builder, ast);
+    translate(&ctr, ast);
 
     // ret i32 0
     builder.build_return(Some(&i32_type.const_int(0, false)))?;
@@ -63,15 +83,10 @@ fn build_main(ast: CoreLang) -> Result<String, Box<dyn Error>> {
     Ok(module.print_to_string().to_string())
 }
 
-fn translate<'ctx>(
-    context: &'ctx Context,
-    module: &Module<'ctx>,
-    builder: &Builder,
-    ast: CoreLang,
-) {
+fn translate(ctr: &CompileController, ast: CoreLang) {
     match ast {
         CoreLang::Apply(func, args) => match args.as_ref() {
-            CoreLang::Tuple(args) => build_apply(context, module, builder, *func, args),
+            CoreLang::Tuple(args) => build_apply(ctr, *func, args),
             _ => panic!("CoreLang::Applyの右辺はタプルである必要があります"),
         },
 
@@ -79,29 +94,25 @@ fn translate<'ctx>(
     };
 }
 
-fn build_apply<'ctx>(
-    context: &'ctx Context,
-    module: &Module<'ctx>,
-    builder: &Builder,
-    func: CoreLang,
-    args: &[CoreLang],
-) {
+fn build_apply(ctr: &CompileController, func: CoreLang, args: &[CoreLang]) {
     if let CoreLang::Symbol(fname) = func {
         if fname == "print" {
             if let CoreLang::Lit(Literal::I32(value)) = args[0] {
-                print_num(&context, &module, &builder, value);
+                print_num(ctr, value);
             } else {
                 panic!("printの引数が数値ではありません");
             }
+        } else if fname == "add" {
+            panic!("addですね");
         } else {
             panic!("未知の関数名です: {}", fname);
         }
     } else if let CoreLang::Fn(param, body) = func {
-        let lambda = create_lambda(context, module, builder, param, *body);
-        let i32_type = context.i32_type();
+        let lambda = create_lambda(ctr, param, *body);
+        let i32_type = ctr.context.i32_type();
 
         // ラムダの呼び出し
-        builder
+        ctr.builder
             .build_call(
                 lambda,
                 &[i32_type.const_int(0, false).into()],
@@ -114,22 +125,20 @@ fn build_apply<'ctx>(
 }
 
 fn create_lambda<'ctx>(
-    context: &'ctx Context,
-    module: &Module<'ctx>,
-    _main_builder: &Builder,
+    ctr: &'ctx CompileController,
     _param: String,
     body: CoreLang,
 ) -> FunctionValue<'ctx> {
     // ラムダ式を実体化
 
-    let builder = context.create_builder();
-    let i32_type = context.i32_type();
+    let builder = ctr.context.create_builder();
+    let i32_type = ctr.context.i32_type();
 
     if let CoreLang::Lit(Literal::I32(value)) = body {
         // define i32 @main() {
         let func_type = i32_type.fn_type(&[], false);
-        let function = module.add_function("lambda", func_type, None);
-        let basic_block = context.append_basic_block(function, "");
+        let function = ctr.module.add_function("lambda", func_type, None);
+        let basic_block = ctr.context.append_basic_block(function, "");
         builder.position_at_end(basic_block);
 
         builder
@@ -142,9 +151,11 @@ fn create_lambda<'ctx>(
     }
 }
 
-fn print_num<'ctx>(context: &'ctx Context, module: &Module<'ctx>, builder: &Builder, value: i32) {
-    let i32_type = context.i32_type();
-    let ptr_type = context.ptr_type(AddressSpace::default());
+fn print_num<'ctx>(ctr: &'ctx CompileController, value: i32) -> CallSiteValue<'ctx> {
+    let module = &ctr.module;
+    let builder = &ctr.builder;
+    let i32_type = ctr.context.i32_type();
+    let ptr_type = ctr.context.ptr_type(AddressSpace::default());
 
     let format_str = module.get_global("num_format").unwrap_or_else(|| {
         // 初回時に文字列リテラルをグローバル変数として追加
@@ -169,5 +180,5 @@ fn print_num<'ctx>(context: &'ctx Context, module: &Module<'ctx>, builder: &Buil
             ],
             "printf_result",
         )
-        .unwrap();
+        .unwrap()
 }
